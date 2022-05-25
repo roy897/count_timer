2e72725875df6e0be45505d9a81949103ef81f19
roy897 <roushangupta85@gmail.com>
2022-05-26 01:28:14 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Timer from './components/Timer';
+import './App.css';
+
+const App = () => {  
+  return (
+    <div className="App">
+      <Timer />       
+    </div>
+  )
+}
+
+export default App;
